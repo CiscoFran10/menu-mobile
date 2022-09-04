ff1239aa6964e9656b1ca1d23295b8b2757b43e6
Thiago <fthiagorodrigues10@gmail.com>
2022-09-04 13:11:09 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const btn = document.querySelector(".btn-mobile");
 
-function toggleNav() {
+function toggleNav(event) {
+  if (event.type === "touchstart") event.preventDefault();
   const nav = document.querySelector(".nav");
   nav.classList.toggle("ativo");
+  const ativo = nav.classList.contains("ativo");
+  event.currentTarget.setAttribute("aria-expanded", ativo);
+  if (ativo) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  }else{
+    event.currentTarget.setAttribute("aria-label","Abrir Menu")
+  }
 }
 
 btn.addEventListener("click", toggleNav);
+btn.addEventListener("touchstart", toggleNav);
